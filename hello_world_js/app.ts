455b1180e16f1b1b70afeaeb1d3f94018742d0d1
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-07-08 16:23:32 +0200

    create docClient within lambda function

--- hello_world_js/app.ts
+++ hello_world_js/app.ts
@@ -2,12 +2,13 @@ import {Context, SQSEvent} from 'aws-lambda';
 import AWS from 'aws-sdk';
 
 // const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
-const docClient = new AWS.DynamoDB.DocumentClient();
+
 
 export const lambdaHandler = (event: SQSEvent, context: Context): void => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
+    const docClient = new AWS.DynamoDB.DocumentClient();
     event.Records.forEach(record => {
         const event = JSON.parse(record.body);
         docClient.put({
