29627543142c600b4b402999ebe294d850b55002
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-07-08 16:03:09 +0200

    Write events to dynamo table

--- hello_world_js/app.ts
+++ hello_world_js/app.ts
@@ -1,8 +1,29 @@
-import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
+import {Context, APIGatewayProxyCallback, SQSEvent} from 'aws-lambda';
+import AWS from 'aws-sdk';
 
-export const lambdaHandler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback): void => {
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
+
+export const lambdaHandler = (event: SQSEvent, context: Context, callback: APIGatewayProxyCallback): void => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+    event.Records.forEach(record => {
+        const event = JSON.parse(record.body);
+        dynamodb.putItem({
+            TableName: "Events",
+            Item: {
+                Channel: event.channel,
+                Event: event.event
+            }
+        }, (error, data) => {
+            if (error) {
+                console.error(`Error: ${error}`)
+            }
+            if (data) {
+                console.log(`Data: ${data}`)
+            }
+        })
+    })
     callback(null, {
         statusCode: 200,
         body: JSON.stringify({
