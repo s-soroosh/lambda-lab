ae474c0eef86ff1310032378b4d3b322db4a8ad7
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-07-08 16:34:32 +0200

    Fix iam role

--- hello_world_js/app.ts
+++ hello_world_js/app.ts
@@ -2,28 +2,28 @@ import {Context, SQSEvent} from 'aws-lambda';
 import AWS from 'aws-sdk';
 
 // const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
-
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 export const lambdaHandler = (event: SQSEvent, context: Context): void => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
-    // const docClient = new AWS.DynamoDB.DocumentClient();
+
     event.Records.forEach(record => {
         const event = JSON.parse(record.body);
-        // docClient.put({
-        //     TableName: "Events",
-        //     Item: {
-        //         Channel: event.channel,
-        //         Event: event.event
-        //     }
-        // }, (error, data) => {
-        //     if (error) {
-        //         console.error(`Error: ${error}`)
-        //     }
-        //     if (data) {
-        //         console.log(`Data: ${data}`)
-        //     }
-        // })
+        docClient.put({
+            TableName: "Events",
+            Item: {
+                Channel: event.channel,
+                Event: event.event
+            }
+        }, (error, data) => {
+            if (error) {
+                console.error(`Error: ${error}`)
+            }
+            if (data) {
+                console.log(`Data: ${data}`)
+            }
+        })
     })
 };
